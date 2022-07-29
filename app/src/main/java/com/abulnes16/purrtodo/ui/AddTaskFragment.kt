68ceb2aaed4c78d8fdada6fac8c78af04e945294
Angel Bulnes <angel_bulnes16@yahoo.com>
2022-07-29 12:05:57 -0600

    Finish some styling and configure the view binding and the navigation of the app

--- app/src/main/java/com/abulnes16/purrtodo/ui/AddTaskFragment.kt
+++ app/src/main/java/com/abulnes16/purrtodo/ui/AddTaskFragment.kt
@@ -5,12 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.abulnes16.purrtodo.R
+import com.abulnes16.purrtodo.databinding.FragmentAddTaskBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,16 +16,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class AddTaskFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentAddTaskBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +29,16 @@ class AddTaskFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_task, container, false)
+        binding = FragmentAddTaskBinding.inflate(inflater, container, false)
+        binding.btnGoBackAddTask.setOnClickListener { goBack() }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddTaskFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddTaskFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    fun goBack(){
+        val action = R.id.action_addTaskFragment_to_homeFragment
+        findNavController().navigate(action)
     }
+
+
 }
